b3f980f5f0c29fd721510ed53b8522482a57fc5e
Namitha Doddangadi Sathyananda <namitha@Namithas-MacBook-Air.local>
2020-09-14 18:04:01 +0530

    changes made

--- weathershopper_min.py
+++ weathershopper_min.py
@@ -1,46 +1,52 @@
 import time
-
 from selenium import webdriver
+from re import search
 
 browser = webdriver.Chrome()
 browser.get("https://weathershopper.pythonanywhere.com/")
 
 browser.find_element_by_xpath('//a[@href="/moisturizer"]').click()
 
+# To get all the products names into a list
 product_list = []
+aloe_list = []
+aloe_list_index = []
 product = browser.find_elements_by_xpath('//div[@class = "text-center col-4"]//p[1]')
-for i in product: 
-    pro_names = i.text
-    list = pro_names.split("\n")
-    product_list.append(list)
+for each_product in product:
+    product_list.append(each_product.text)
 
-print(product_list)
+for each in product_list:
+    if search("Aloe",each):
+        aloe_list.append(each)
+        aloe_index= product_list.index(each)
+        aloe_list_index.append(aloe_index)
 
-
+print(aloe_list)
+print(aloe_list_index)
+'''
+# To get all the corresponding products price into a list
 price_list =[]
 price = browser.find_elements_by_xpath('//div[@class = "text-center col-4"]//p[2]')
-for i in price:
-    price_num = i.text
+for each_price in price:
+    price_num = each_price.text
     num = price_num.rsplit(' ',1)[-1]
-    list = int(num.split("\n")[0])
-    price_list.append(list)
+    listOfInt = int(num.split("\n")[0])
+    price_list.append(listOfInt)
 
 
-print(price_list)
-
+# Checking product that is of low price.
 low_price = min(price_list)
 min_index = price_list.index(low_price)
-print(min_index)
 
-moist =str(product_list[min_index])[2:-2]
-print(moist)
-time.sleep(3)
-
-b = browser.find_element_by_xpath('//button[@onclick = "addToCart('%s',%s)"%(moist,low_price)]').click()
+# getting product name for the corresponding index
+low_priced_moisturiser = product_list[min_index]
+low_priced_moisturiser = "'"+ low_priced_moisturiser +"'"
+print(low_priced_moisturiser)
 
 
-print(b)
- 
+browser.find_element_by_xpath('//button[@onclick ="addToCart(%s,%s)"]'%(low_priced_moisturiser,str(low_price))).click()
+
+''' 
 time.sleep(3)
 
 browser.quit()
