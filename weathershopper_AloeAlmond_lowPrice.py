e7f2a3f695ba1480ed18e35e290b2d24bc815bc7
Namitha Doddangadi Sathyananda <namitha@Namithas-MacBook-Air.local>
2020-09-15 18:50:33 +0530

    To find lowest price product in Aloe or Almond category

--- weathershopper_AloeAlmond_lowPrice.py
+++ weathershopper_AloeAlmond_lowPrice.py
@@ -1,3 +1,15 @@
+'''
+SCOPE:
+1) Launch Chrome driver
+2) Navigate to http://qainterview.pythonanywhere.com/
+3) Fill all the text field
+4) Click on Calcutate button
+5) Validate the result.
+6) Close the browser
+'''
+
+
+
 import time
 from selenium import webdriver
 from re import search
@@ -19,15 +31,15 @@ def moisturizer_type(moist):
     for each_button in add_button:
         details = each_button.get_attribute("onclick")
         print(details)
-    
+    # checking for the "Aloe or Almond" word in the string
         if search(moist,details):
             product.append(details.split("'")[1:-1][0])
             price.append(int(details.split(",")[1].strip(")")))
 
+    # passing list of price to lowest function and fetching the result and its index
     lowest_Product = lowest(price)
     index_min = price.index(lowest_Product)
     
-
     # getting product name for the corresponding index
     low_priced_moisturiser = product[index_min]
     print("\n",low_priced_moisturiser,lowest_Product,"\n")
@@ -36,12 +48,13 @@ def moisturizer_type(moist):
     browser.find_element_by_xpath('//button[@onclick ="addToCart(%s,%s)"]'%(low_priced_moisturiser,str(lowest_Product))).click()
 
 
-
+# getting the input from the console to check for Aloe or Almond producd.
 moisturizer = input("Enter the ingredient in moisturiser (Aloe or Almond):  ")
     
 browser = webdriver.Chrome()
 browser.get("https://weathershopper.pythonanywhere.com/")
 
+# passing the input from the console to the moisturizer_type function.
 moisturizer_type(moisturizer)
 
 time.sleep(5)
